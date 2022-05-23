8282155dee2d32f9e320d45edf3384187a888384
Hélvio Filho <helviosvf@gmail.com>
2022-05-22 23:36:21 -0300

    Feat: login screen created to give security to the application

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,23 +1,30 @@
-import React, { useEffect, useState } from 'react';
-import { Alert, Text, View } from 'react-native';
+import React from 'react';
+import { Alert, StatusBar } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
 
 import * as LocalAuthentication from "expo-local-authentication";
 
+import Logo from '../../assets/logo.svg';
+
+import {
+  Container,
+  Title,
+  ContainerTop,
+  FieldContainer,
+  Warning,
+} from './styles';
+
 import { userRoot } from '../../hooks/auth';
+import { Button } from '../../components/Form/Button';
+import { RFValue } from 'react-native-responsive-fontsize';
 
 export function Login() {
-  const [isBiometricSupported, setIsBiometricSupported] = useState(false);
-
   const { userAlreadyExist } = userRoot();
-
   const { navigate } = useNavigation();
 
   async function handleBiometricAuth() {
     try {
       const trigger = await LocalAuthentication.getEnrolledLevelAsync();
-      console.log(trigger);
       if (trigger === 0) {
         Alert.alert("Atenção", "Seu celular precisa de uma senha pin ou padrão para utilizar esse aplicativo!")
         return false;
@@ -27,7 +34,8 @@ export function Login() {
         promptMessage: 'Entrar usando a biometria?',
         fallbackLabel: 'Entre com a senha',
       });
-      if (biometricAuth) {
+
+      if (biometricAuth.success) {
         if (userAlreadyExist) {
           navigate('Home');
         } else {
@@ -39,19 +47,38 @@ export function Login() {
     }
   }
 
-  useEffect(() => {
-    async function compatible() {
-      const compatible = await LocalAuthentication.hasHardwareAsync();
-      setIsBiometricSupported(compatible);
-    }
-    compatible();
-  }, []);
-
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <TouchableOpacity onPress={() => handleBiometricAuth()}>
-        <Text>Botão</Text>
-      </TouchableOpacity>
-    </View>
+    <Container>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <ContainerTop>
+        <Logo
+          width="200"
+          height="150"
+        />
+      </ContainerTop>
+      <FieldContainer>
+        <Title>
+          Para manter as suas senhas seguras{'\n'}
+          sempre vamos pedir uma senha{'\n'}
+          para acessar o aplicativo.
+        </Title>
+        <Button
+          activeOpacity={0.8}
+          onPress={handleBiometricAuth}
+          title="Usar senha do celular"
+          style={{
+            width: RFValue(250),
+            marginVertical: 25,
+          }}
+        />
+        <Warning>
+          ** Atenção! Essa senha é a mesma forma de validação que você usa para desbloquear o celular
+        </Warning>
+      </FieldContainer>
+    </Container>
   );
 }
---
+++ src/screens/Login/styles.ts
@@ -0,0 +1,41 @@
+import styled from 'styled-components/native';
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
+
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  background-color: #508bfc;
+`;
+
+export const ContainerTop = styled.View`
+  width: 100%;
+  height: 35%;
+  justify-content: space-around;
+  align-items: center;
+  background-color: #F2F3F5;
+  padding-top: ${RFPercentage(3)}px;
+  margin-bottom: 25px;
+`;
+
+export const FieldContainer = styled.View`
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  padding-top: 20px;
+`;
+
+export const Title = styled.Text`
+  font-size: ${RFValue(20)}px;
+  line-height: ${RFValue(26)}px;
+  font-family: 'Rubik_300Light';
+  color: #F2F3F5;
+  margin-bottom: 5px;
+`;
+
+export const Warning = styled.Text`
+  font-size: ${RFValue(12)}px;
+  color: #d1d1d1;
+  line-height: ${RFValue(17)}px;
+  font-family: 'Rubik_300Light';
+  padding: 20px;
+`;
