af49cadbb65b1582fb09fd2b42d2504c1fd75593
Hélvio Filho <helviosvf@gmail.com>
2022-05-24 19:15:38 -0300

    Feat: Created generic warning modal

---
+++ src/components/WarningModal/index.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+
+import {
+  Container,
+  ContainerModal,
+  Close,
+  IconX,
+  Message,
+  Footer,
+  Button,
+  Title,
+} from './styles';
+
+interface WarningModalProps {
+  message: string;
+  button: {
+    title: string;
+    color: string;
+    close: boolean;
+  }[];
+  closeModal: () => void;
+  primaryFunction?: () => void;
+}
+
+export function WarningModal({ message, button, closeModal, primaryFunction }: WarningModalProps) {
+  return (
+    <Container>
+      <ContainerModal>
+        <Close
+          onPress={closeModal}
+        >
+          <IconX
+            name="md-close-circle-outline"
+            size={30}
+            color="white"
+          />
+        </Close>
+        <Message>
+          {message}
+        </Message>
+        <Footer itens={button.length}>
+          {
+            button.map((item) => (
+              <Button
+                key={item.title}
+                color={item.color}
+                onPress={item.close ? closeModal : primaryFunction}
+
+              >
+                <Title>{item.title}</Title>
+              </Button>
+            )
+            )
+          }
+        </Footer>
+      </ContainerModal>
+    </Container>
+  );
+}
---
+++ src/components/WarningModal/styles.ts
@@ -0,0 +1,78 @@
+import styled from 'styled-components/native';
+import { Ionicons } from '@expo/vector-icons';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+interface buttonProps {
+  color: string;
+}
+
+interface FooterProps {
+  itens: number;
+}
+
+export const Container = styled.View`
+  flex: 1;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+
+  background-color: rgba(0,0,0,0.4);
+`;
+
+export const IconX = styled(Ionicons)``;
+
+export const ContainerModal = styled.View`
+  width: 90%;
+  height: 160px;
+  justify-content: center;
+  align-items: center;
+  
+  background-color: #508bfc;
+  
+  border-radius: 20px;
+  padding: 10px;
+`;
+
+export const Close = styled.TouchableOpacity`
+  position: absolute;
+  top: 5px;
+  right: 5px;
+  padding: 5px;
+`;
+
+export const Message = styled.Text`
+  width: 90%;
+  font-size: ${RFValue(14)}px;
+  
+  color: #EBF2FF;
+  
+  padding-top: 20px;
+`;
+
+export const Footer = styled.View<FooterProps>`
+  width: 90%;
+  height: 50px;
+
+  padding: 16px 35px 0;
+  
+  flex-direction: row;
+  justify-content: ${({ itens }) => itens === 1 ? "center" : "space-between"} ;
+`;
+
+export const Button = styled.TouchableOpacity<buttonProps>`
+  width: 90px;
+  height: 40px;
+
+  justify-content: center;
+  align-items: center;
+  
+  background-color: ${({ color }) => color};
+  
+  border-radius: 10px;
+`;
+
+export const Title = styled.Text`
+  font-size: ${RFValue(13)}px;
+  
+  color: white;
+`;
